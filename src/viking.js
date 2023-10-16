e4bff7cd4b95158a6a0bba066dee1b1969c85e03
Alba Garcia Gonzalez <garciagonzalezalba@gmail.com>
2023-10-16 09:41:07 +0200

    better done

--- src/viking.js
+++ src/viking.js
@@ -63,13 +63,13 @@ class War {
         this.vikingArmy = [];
         this.saxonArmy = []; 
     }
-    addViking(Viking)
+    addViking(viking)
     {
-        this.vikingArmy.push(Viking);
+        this.vikingArmy.push(viking);
     }
-    addSaxon(Saxon)
+    addSaxon(saxon)
     {
-        this.saxonArmy.push(Saxon)
+        this.saxonArmy.push(saxon)
     }
     vikingAttack()
     {
@@ -142,34 +142,33 @@ class War {
         }
     }
 }
-
+let viking="hola"
 let vikingOne = new Viking ("juanix", 10, 5);
+let vikingTwo = new Viking ("albix", 10, 5);
+let vikingThree = new Viking ("Olivix", 10, 5);
+let vikingFour = new Viking ("Arturix", 10, 5);
+let vikingFive = new Viking ("Micaelix", 10, 5);
+let saxonOne = new Saxon(20, 2);
+let saxonTwo = new Saxon(20, 2);
+let saxonThree = new Saxon(20, 2);
+let saxonFour = new Saxon(20, 2);
+let saxonFive = new Saxon(20, 2);
 
-// let vikingTwo = new Viking ("albix", 10, 5);
-// let vikingThree = new Viking ("Olivix", 10, 5);
-// let vikingFour = new Viking ("Arturix", 10, 5);
-// let vikingFive = new Viking ("Micaelix", 10, 5);
-// let saxonOne = new Saxon(20, 2);
-// let saxonTwo = new Saxon(20, 2);
-// let saxonThree = new Saxon(20, 2);
-// let saxonFour = new Saxon(20, 2);
-// let saxonFive = new Saxon(20, 2);
+let firstWar = new War();
 
-// let firstWar = new War();
+firstWar.addViking(vikingOne);
+firstWar.addViking(vikingTwo);
+firstWar.addViking(vikingThree);
+firstWar.addViking(vikingFour);
+firstWar.addViking(vikingFive);
 
-// firstWar.addViking(vikingOne);
-// firstWar.addViking(vikingTwo);
-// firstWar.addViking(vikingThree);
-// firstWar.addViking(vikingFour);
-// firstWar.addViking(vikingFive);
+firstWar.addSaxon(saxonOne);
+firstWar.addSaxon(saxonTwo);
+firstWar.addSaxon(saxonThree);
+firstWar.addSaxon(saxonFour);
+firstWar.addSaxon(saxonFive);
 
-// firstWar.addSaxon(saxonOne);
-// firstWar.addSaxon(saxonTwo);
-// firstWar.addSaxon(saxonThree);
-// firstWar.addSaxon(saxonFour);
-// firstWar.addSaxon(saxonFive);
-
-// firstWar.vikingAttack();
+firstWar.vikingAttack();
 
 
 
